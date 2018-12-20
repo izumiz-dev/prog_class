08042cfd051785fd71622586ffca2428b3f1517f
polyiz <42127166+polyiz@users.noreply.github.com>
2018-12-21 00:07:52 +0900

    関数化など
    関数化、無駄にロード画面の実装

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX 1024
+#include <stdbool.h>
+#include <windows.h>
 
 typedef struct Dict{
     char en[32];
@@ -10,41 +11,69 @@ typedef struct Dict{
 
 Dict words[100];
 
+
+void viewDict(void);
+int readDict(char *);
+void loadingBar(void);
+
 int main(int argc, char *argv[]) {
-    FILE *fp;
-    char *fname = "en-ja.csv";
-    char temp[32];
+
+    if (argc == 1) {
+        printf("Loading build-in en-ja.csv\n");
+        readDict("en-ja.csv");
+    } else if (argc == 2) {
+        printf("Load %s\n", argv[1]);
+        readDict(argv[1]);
+    }
+
     int i = 0;
-
-    fp = fopen(fname, "r");
-
-    if (fp == NULL) {
-        printf("%s does not exist.\n", fname);
-        return -1;
-    } else {
-        printf("%s has opened.\n\n\n", fname);
-    }
-
-    for (i=0; i < 32; i++) {
-        fscanf(fp, "%[^,],%s", &words[i].en, &words[i].ja);
-    }
+    return (0);
+}
 
 
-    // do {
-    //     fscanf(fp, "%[^,],%s", &words[i].en, &words[i].ja);
-    //     i++;
-    // } while (strlen(words[i].en) == 0);
-
-
-    printf("[%s] [%s]\n\n", words[24].en, words[24].ja);
-
-
+void viewDict(void) {
+    // 読み込んだ.csvの内容をprintfするサブルーチン 
+    int i = 0;
     i = 0;
     while (strlen(words[i].en) != 0) {
         printf("%s %s", words[i].en, words[i].ja);
         i++;
     }
+}
+
+
+int readDict(char *inputFile) {
+    // csvの読み込みを行うサブルーチン
+    int i = 0;
+    FILE *fp;
+    char *fname = inputFile;
+    fp = fopen(fname, "r");
+
+    if (fp == NULL) {
+        printf("%s does not exist.\n", fname);
+        return(false);
+    } else {
+        loadingBar();
+        printf("File load complete!!\n");
+        return(true);
+    }
     
+    while (1) {
+        fscanf(fp, "%[^,],%s", &words[i].en, &words[i].ja);
+        if (strlen(words[i].en) == 1)
+            break;
+        i++;
+    }
     fclose(fp);
-    return (0);
+}
+
+void loadingBar(void) {
+    Sleep(500);
+    int i = 0;
+    for (i = 0; i < 60; i++) {
+        printf("*");
+        Sleep(20);
+    }
+    printf("\n");
+    Sleep(100);
 }
